6013f4923756c0817ffd43794381b22632ef9a9b
Henrique <Henrique@192.168.25.157>
2018-04-02 07:53:06 -0300

    alteracoes no menu

--- src/java/newpackage/mostraheader.java
+++ src/java/newpackage/mostraheader.java
@@ -1,19 +1,21 @@
-package newpackage;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package newpackage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+@WebServlet(name = "mostraheader", urlPatterns = {"/mostraheader"})
 public class mostraheader extends HttpServlet {
 
     /**
@@ -25,22 +27,6 @@ public class mostraheader extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MostraHeader</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MostraHeader at " + request.getHeaderNames() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -52,33 +38,19 @@ public class mostraheader extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Headers<hr/>");
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			out.print("Header Name: <em>" + headerName);
+			String headerValue = request.getHeader(headerName);
+			out.print("</em>, Header Value: <em>" + headerValue);
+			out.println("</em><br/>");
+		}
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+}
 }
